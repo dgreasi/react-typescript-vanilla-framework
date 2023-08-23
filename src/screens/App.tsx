2ebcfc4e786cf90d24703b00504a48cdda8e8d3a
Dgreasidis <dgreasidis+github@gmail.com>
2023-08-23 15:38:46 +0300

    feat: update styling of main screen

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from '../styles/logo.svg';
-import './App.css';
+import './App.scss';
 import APIProvider from '../api/APIProvider';
-import { Posts } from '../components/Posts';
+import { PostList } from '../components/PostList';
 
 function App() {
   return (
     <APIProvider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          <Posts />
-        </header>
+        <PostList />
       </div>
     </APIProvider>
   );
